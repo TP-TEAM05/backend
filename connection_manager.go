d6187c9b9d139d38b831fbb43a64ecb812a40938
MilonovaV <71688553+MilonovaV@users.noreply.github.com>
2023-11-20 19:54:57 +0100

    proof of concept - subscriber communication with integration module

---
+++ connection_manager.go
@@ -0,0 +1,302 @@
+package main
+
+import (
+	"fmt"
+	_ "net/http/pprof"
+	"time"
+
+	"github.com/jftuga/geodist"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+// Keeps the connection with Integration module alive and ensures there is a connection
+func sendKeepAlives(connection *IntegrationModuleConnection, interval float32) {
+	for {
+		var datagram IDatagram
+
+		// Connect
+		datagram = &ConnectDatagram{
+			BaseDatagram: BaseDatagram{Type: "connect"},
+		}
+
+		acknowledged := connection.WriteAcknowledgedDatagram(datagram, 3, true)
+		if !acknowledged {
+			fmt.Printf("Could not connect to %v\n", connection.ServerAddress)
+			continue
+		}
+
+		// Start sending keep-alives
+		for range time.Tick(time.Second * time.Duration(interval)) {
+			// Keep Alive
+			datagram = &KeepAliveDatagram{
+				BaseDatagram: BaseDatagram{Type: "keepalive"},
+			}
+
+			acknowledged = connection.WriteAcknowledgedDatagram(datagram, 3, true)
+			if !acknowledged {
+				fmt.Printf("Could not send keep-alive to %v\n", connection.ServerAddress)
+				break
+			}
+		}
+	}
+}
+
+// Subscribes to the Integration Module and checks that it is live, subscriptionContent can be "vehicles" or "notifications"
+func maintainSubscription(connection *IntegrationModuleConnection, subscriptionContent string, subscriptionInterval float32, checkInterval float32) {
+	if connection == nil {
+		panic("Connection cannot be nil")
+	}
+
+	var datagram IDatagram
+
+	datagram = &SubscribeDatagram{
+		BaseDatagram: BaseDatagram{
+			Type: "subscribe",
+		},
+		Content:  subscriptionContent,
+		Interval: subscriptionInterval,
+	}
+
+	for {
+		// Subscribe
+		acknowledged := connection.WriteAcknowledgedDatagram(datagram, 3, true)
+		if !acknowledged {
+			fmt.Printf("Could not subscribe to %v\n", connection.ServerAddress)
+		}
+
+		// Time to get first updates
+		time.Sleep(time.Second * time.Duration(subscriptionInterval*2))
+
+		// Check subscription is active
+		for range time.Tick(time.Second * time.Duration(checkInterval)) {
+
+			var lastDatagram IDatagram
+			switch subscriptionContent {
+			case "vehicles":
+				if connection.LastOnUpdateVehiclesDatagram != nil {
+					lastDatagram = connection.LastOnUpdateVehiclesDatagram
+				}
+			case "notifications":
+				if connection.LastOnUpdateNotificationsDatagram != nil {
+					lastDatagram = connection.LastOnUpdateNotificationsDatagram
+				}
+			}
+
+			if lastDatagram == nil {
+				break
+			}
+
+			lastUpdateTime, err := time.Parse(TimestampFormat, lastDatagram.GetTimestamp())
+			if err != nil {
+				break
+			}
+			secondsSinceLastUpdate := float32(time.Now().Sub(lastUpdateTime).Seconds())
+			if secondsSinceLastUpdate > subscriptionInterval*1.1 {
+				break
+			}
+		}
+	}
+}
+
+// notificationsType can be "head_collision" or "chain_collision" or "crossroad"
+func simulateNotifications(connection *IntegrationModuleConnection, notificationsType string) {
+	for {
+		vehiclesDatagram := <-connection.OnUpdateVehicles
+		vehicles := vehiclesDatagram.Vehicles
+		for i := 0; i < len(vehicles)-1; i++ {
+			for j := i + 1; j < len(vehicles); j++ {
+				vehicleA := vehicles[i]
+				vehicleB := vehicles[j]
+
+				_, km := geodist.HaversineDistance(
+					geodist.Coord{Lat: float64(vehicleA.Position.Lat), Lon: float64(vehicleA.Position.Lon)},
+					geodist.Coord{Lat: float64(vehicleB.Position.Lat), Lon: float64(vehicleB.Position.Lon)})
+
+				metersDistance := km * 1000.0
+
+				const SafeDistance = 100
+
+				if notificationsType == "head_collision" {
+					if metersDistance < SafeDistance {
+
+						level := "info"
+						if metersDistance < SafeDistance/2.0 {
+							level = "warning"
+						}
+						if metersDistance < SafeDistance/4.0 {
+							level = "danger"
+						}
+
+						datagram := &HeadCollisionNotifyDatagram{
+							NotifyDatagram: NotifyDatagram{
+								BaseDatagram: BaseDatagram{
+									Type: "notify",
+								},
+								VehicleId:   vehicleA.Id,
+								Level:       level,
+								ContentType: "head_collision",
+							},
+							Content: HeadCollisionNotificationContent{
+								TargetVehicleId:      vehicleB.Id,
+								TimeToCollision:      float32(metersDistance / 100.0),
+								MaxSpeedExceededBy:   10,
+								BreakingDistanceDiff: float32(metersDistance),
+							},
+						}
+						connection.WriteAcknowledgedDatagram(datagram, 2, true)
+
+						datagram = &HeadCollisionNotifyDatagram{
+							NotifyDatagram: NotifyDatagram{
+								BaseDatagram: BaseDatagram{
+									Type: "notify",
+								},
+								VehicleId:   vehicleB.Id,
+								Level:       level,
+								ContentType: "head_collision",
+							},
+							Content: HeadCollisionNotificationContent{
+								TargetVehicleId:      vehicleA.Id,
+								TimeToCollision:      float32(metersDistance / 100.0),
+								MaxSpeedExceededBy:   10,
+								BreakingDistanceDiff: float32(metersDistance),
+							},
+						}
+						connection.WriteAcknowledgedDatagram(datagram, 2, true)
+					}
+
+				} else if notificationsType == "chain_collision" {
+					if metersDistance < SafeDistance {
+
+						level := "info"
+						if metersDistance < SafeDistance/2.0 {
+							level = "warning"
+						}
+						if metersDistance < SafeDistance/4.0 {
+							level = "danger"
+						}
+
+						datagram := &ChainCollisionNotifyDatagram{
+							NotifyDatagram: NotifyDatagram{
+								BaseDatagram: BaseDatagram{
+									Type: "notify",
+								},
+								VehicleId:   vehicleA.Id,
+								Level:       level,
+								ContentType: "chain_collision",
+							},
+							Content: ChainCollisionNotificationContent{
+								TargetVehicleId:     vehicleB.Id,
+								CurrentDistance:     float32(metersDistance),
+								RecommendedDistance: SafeDistance,
+							},
+						}
+						connection.WriteAcknowledgedDatagram(datagram, 2, true)
+
+						datagram = &ChainCollisionNotifyDatagram{
+							NotifyDatagram: NotifyDatagram{
+								BaseDatagram: BaseDatagram{
+									Type: "notify",
+								},
+								VehicleId:   vehicleB.Id,
+								Level:       level,
+								ContentType: "chain_collision",
+							},
+							Content: ChainCollisionNotificationContent{
+								TargetVehicleId:     vehicleA.Id,
+								CurrentDistance:     float32(metersDistance),
+								RecommendedDistance: SafeDistance,
+							},
+						}
+						connection.WriteAcknowledgedDatagram(datagram, 2, true)
+					}
+				} else if notificationsType == "crossroad" {
+					if metersDistance < SafeDistance {
+
+						level := "info"
+						if metersDistance < SafeDistance/2.0 {
+							level = "warning"
+						}
+						if metersDistance < SafeDistance/4.0 {
+							level = "danger"
+						}
+
+						datagram := &CrossroadNotifyDatagram{
+							NotifyDatagram: NotifyDatagram{
+								BaseDatagram: BaseDatagram{
+									Type: "notify",
+								},
+								VehicleId:   vehicleA.Id,
+								Level:       level,
+								ContentType: "crossroad",
+							},
+							Content: CrossroadNotificationContent{
+								Text:       "Pojdeš prvý.",
+								Order:      1,
+								RightOfWay: true,
+							},
+						}
+						connection.WriteAcknowledgedDatagram(datagram, 2, true)
+
+						datagram = &CrossroadNotifyDatagram{
+							NotifyDatagram: NotifyDatagram{
+								BaseDatagram: BaseDatagram{
+									Type: "notify",
+								},
+								VehicleId:   vehicleB.Id,
+								Level:       level,
+								ContentType: "crossroad",
+							},
+							Content: CrossroadNotificationContent{
+								Text:       "Pojdeš druhý.",
+								Order:      2,
+								RightOfWay: false,
+							},
+						}
+						connection.WriteAcknowledgedDatagram(datagram, 2, true)
+					}
+				} else if notificationsType == "generic" {
+					if metersDistance < SafeDistance {
+
+						level := "info"
+						if metersDistance < SafeDistance/2.0 {
+							level = "warning"
+						}
+						if metersDistance < SafeDistance/4.0 {
+							level = "danger"
+						}
+
+						datagram := &GenericNotifyDatagram{
+							NotifyDatagram: NotifyDatagram{
+								BaseDatagram: BaseDatagram{
+									Type: "notify",
+								},
+								VehicleId:   vehicleA.Id,
+								Level:       level,
+								ContentType: "generic",
+							},
+							Content: GenericNotificationContent{
+								Text: "Prajeme príjemnú jazdu.",
+							},
+						}
+						connection.WriteAcknowledgedDatagram(datagram, 2, true)
+
+						datagram = &GenericNotifyDatagram{
+							NotifyDatagram: NotifyDatagram{
+								BaseDatagram: BaseDatagram{
+									Type: "notify",
+								},
+								VehicleId:   vehicleB.Id,
+								Level:       level,
+								ContentType: "generic",
+							},
+							Content: GenericNotificationContent{
+								Text: "Prajeme príjemnú jazdu.",
+							},
+						}
+						connection.WriteAcknowledgedDatagram(datagram, 2, true)
+					}
+				}
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net"
+	"os"
 	"recofiit/routes"
 	"recofiit/services"
 	wsservice "recofiit/services/wsService"
@@ -9,6 +12,28 @@ import (
 )
 
 func main() {
+	// --------- Subscribe to Integration Module ---------------
+
+	ips, err := net.LookupIP("car-integration")
+	if err != nil || len(ips) == 0 {
+		fmt.Printf("Could not resolve or find hostname %v\n", err)
+		os.Exit(1)
+	}
+
+	port := 5050
+	serverAddress := net.UDPAddr{Port: port, IP: ips[0]}
+
+	connection := NewIntegrationModuleConnection(&serverAddress)
+	connection.Establish()
+
+	// Send keep-alives and check whether subscription is active periodically
+	go sendKeepAlives(connection, 10)
+
+	// Handle vehicle subscriptions
+	go maintainSubscription(connection, "vehicles", 5, 10)
+
+	// --------- Subscribe to Integration Module ---------------
+
 	router := gin.Default()
 	router.SetTrustedProxies([]string{""})
 	services.Register()
