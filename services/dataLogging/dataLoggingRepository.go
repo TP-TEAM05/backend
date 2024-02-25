4e3f473c2709c84aa76db0c2847a5ffc2ab61901
Vanesa <vanesa@Notebook.local>
2024-02-25 10:58:48 +0100

    adding migrations, revert deleting data2

--- services/dataLogging/dataLoggingRepository.go
+++ services/dataLogging/dataLoggingRepository.go
@@ -14,23 +14,28 @@ func LogData(datagram models.UpdateVehicleDatagram) {
 		panic("Failed to create session and car session")
 	}
 
-	SaveMeasurement(*measurementController, carSessionID, "LONGITUDE", datagram.Vehicle.Latitude)
-	SaveMeasurement(*measurementController, carSessionID, "LATITUDE", datagram.Vehicle.Longitude)
-	SaveMeasurement(*measurementController, carSessionID, "DISTANCE_ULTRASONIC", datagram.Vehicle.DistanceUltrasonic)
-	SaveMeasurement(*measurementController, carSessionID, "DISTANCE_LIDAR", datagram.Vehicle.DistanceLidar)
-	SaveMeasurement(*measurementController, carSessionID, "SPEED_FRONT_LEFT", datagram.Vehicle.SpeedFrontLeft)
-	SaveMeasurement(*measurementController, carSessionID, "SPEED_FRONT_RIGHT", datagram.Vehicle.SpeedFrontRight)
-	SaveMeasurement(*measurementController, carSessionID, "SPEED_REAR_LEFT", datagram.Vehicle.SpeedRearLeft)
-	SaveMeasurement(*measurementController, carSessionID, "SPEED_REAR_RIGHT", datagram.Vehicle.SpeedRearRight)
+	SaveMeasurement(*measurementController, carSessionID, "GPS", datagram.Vehicle.Latitude, datagram.Vehicle.Longitude)
+	SaveMeasurement(*measurementController, carSessionID, "DISTANCE_ULTRASONIC", datagram.Vehicle.DistanceUltrasonic, nil)
+	SaveMeasurement(*measurementController, carSessionID, "DISTANCE_LIDAR", datagram.Vehicle.DistanceLidar, nil)
+	SaveMeasurement(*measurementController, carSessionID, "SPEED_FRONT_LEFT", datagram.Vehicle.SpeedFrontLeft, nil)
+	SaveMeasurement(*measurementController, carSessionID, "SPEED_FRONT_RIGHT", datagram.Vehicle.SpeedFrontRight, nil)
+	SaveMeasurement(*measurementController, carSessionID, "SPEED_REAR_LEFT", datagram.Vehicle.SpeedRearLeft, nil)
+	SaveMeasurement(*measurementController, carSessionID, "SPEED_REAR_RIGHT", datagram.Vehicle.SpeedRearRight, nil)
 }
 
-func SaveMeasurement(measurementController MeasurementController, carSessionID uint, sensorName string, data float32) {
+func SaveMeasurement(measurementController MeasurementController, carSessionID uint, sensorName string, data1 float32, data2 *float32) {
 	sensorID, err := measurementController.GetSensorID("BaseSensor", sensorName)
 	if err != nil {
 		return
 	}
 
-	measurement := models.Measurement{CarSessionID: carSessionID, CreatedAt: &time.Time{}, SensorID: sensorID, Data: data}
+	var data2Value float32 = 0
+
+	if data2 != nil {
+		data2Value = *data2
+	}
+
+	measurement := models.Measurement{CarSessionID: carSessionID, CreatedAt: &time.Time{}, SensorID: sensorID, Data1: data1, Data2: data2Value}
 	err = measurementController.InsertMeasurement(measurement)
 	if err != nil {
 		return
